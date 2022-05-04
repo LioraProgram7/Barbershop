9a615fe3c0962f87ef5e0a990548b33f0b3544aa
LioraProgram7 <sv.kiselis@gmail.com>
2022-05-04 15:25:04 +0300

    adaptive heager, get-started, our-histiry sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,11 @@
 $(function () {
 
-new Swiper('.reviews__wrapper');
+  new Swiper('.reviews__wrapper');
+
+
+  $('.menu__btn, .menu a').on('click', function () {
+    $('.menu__list, .menu__btn').toggleClass('active');
+  });
 
 
 });
